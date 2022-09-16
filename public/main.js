504e7201026c5a3b1fc076201b341281e9765630
Trung Nguyen <trung1704ptit@gmail.com>
2022-09-16 12:10:36 +0700

    update

--- public/main.js
+++ public/main.js
@@ -1,21 +1,6 @@
 (function () {
-    const isIOS = (function () {
-        var iosQuirkPresent = function () {
-            var audio = new Audio();
-    
-            audio.volume = 0.5;
-            return audio.volume === 1;   // volume cannot be changed from "1" on iOS 12 and below
-        };
-    
-        var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-        var isAppleDevice = navigator.userAgent.includes('Macintosh');
-        var isTouchScreen = navigator.maxTouchPoints >= 1;   // true for iOS 13 (and hopefully beyond)
-    
-        return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
-    })();
-
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
-
 
     // check device is iOS or Mac OS
     if (isSafari || isIOS) {
